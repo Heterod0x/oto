88a4107c60c81fa2a232e555d879986e0346f190
t-nakatani <tomoya.12@icloud.com>
2025-05-03 11:27:01 +0900

    feat: add cors middleware

--- backend/api/user_api/main.py
+++ backend/api/user_api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.presentation.routing.conversation_router import conversation_router
 from app.presentation.routing.user_profile import user_profile_router
@@ -7,6 +8,14 @@ app = FastAPI()
 
 app.include_router(conversation_router)
 app.include_router(user_profile_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
