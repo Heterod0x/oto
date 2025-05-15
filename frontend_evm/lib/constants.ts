bba7287a7b37ca46180602e2080d9aa20ea73a21
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-05-15 23:20:31 +0900

    feat: update API_BASE_URL to production server

--- frontend_evm/lib/constants.ts
+++ frontend_evm/lib/constants.ts
@@ -1,5 +1,5 @@
 // Backend API BASE URL
-export const API_BASE_URL = "http://localhost:8008";
+export const API_BASE_URL = "http://5.104.83.184";
 // Contract address(base sepolia)
 export const CONTRACT_BASE_SEPOLIA_ADDRESS = "0x0a730733Ee85466278Cd31F452165A30971Ecdbd";
 // Contract address(base)
