a571097c7547883caf367ccd74621d07db2a0258
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-05-17 00:17:38 +0900

    refactor: update icon paths and types in manifest for consistency

--- frontend_evm/app/manifest.ts
+++ frontend_evm/app/manifest.ts
@@ -13,14 +13,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/icons/logo.jpeg",
         sizes: "192x192",
-        type: "image/png",
+        type: "image/jpeg",
       },
       {
-        src: "/icons/icon-512x512.png",
+        src: "/icons/logo.jpeg",
         sizes: "512x512",
-        type: "image/png",
+        type: "image/jpeg",
       },
     ],
     orientation: "portrait",
