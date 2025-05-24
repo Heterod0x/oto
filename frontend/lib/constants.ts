654b849ca7cc5834c809b02cb7487c0808377f7d
t-nakatani <tomoya.12@icloud.com>
2025-05-24 13:32:22 +0900

    feat: replace ip endpoint with ngrok endpoint

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,4 +1,4 @@
 // Backend API BASE URL
-export const API_BASE_URL = "https://5.104.83.184";
+export const API_BASE_URL = "https://c077816c97e1.ngrok.app";
 // Contract address
 export const CONTRACT_ADDRESS = "";
