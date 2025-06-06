4df6e3c44b1829320662609f27e2f1c3ee759326
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-06-06 22:56:55 +0900

    add base frontend component

---
+++ frontend_vapi/README.md
@@ -0,0 +1,42 @@
+# Oto VAPI Hackathon Frontend
+
+## How to work
+
+- setup
+
+  create `.env`
+
+  ```bash
+  cp .env.example .env.local
+  ```
+
+  set set below value
+
+  ```txt
+  NEXT_PUBLIC_PRIVY_APP_ID=
+  PRIVY_APP_SECRET=
+  ```
+
+- format
+
+  ```bash
+  pnpm run format
+  ```
+
+- install
+
+  ```bash
+  pnpm install
+  ```
+
+- build
+
+  ```bash
+  pnpm run build
+  ```
+
+- dev
+
+  ```bash
+  pnpm run dev
+  ```
---
+++ frontend_vapi/next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+import withPWA from "next-pwa";
+
+const nextConfig = {
+  output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
+
+export default pwaConfig;
